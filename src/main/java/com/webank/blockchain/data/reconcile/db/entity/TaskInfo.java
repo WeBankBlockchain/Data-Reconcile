0c263b6f4e2736fd7993fd7ceee16e6a4d78afc8
卡拉马佐夫 <973459080@qq.com>
2020-11-13 14:38:06 +0800

    title change

--- src/main/java/com/webank/blockchain/data/reconcile/db/entity/TaskInfo.java
+++ src/main/java/com/webank/blockchain/data/reconcile/db/entity/TaskInfo.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2020 Webank.
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.webank.blockchain.data.reconcile.db.entity;
 
 import lombok.Data;
@@ -5,21 +18,6 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-/*
- * Copyright 2014-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import javax.persistence.*;
 import java.util.Date;
 
